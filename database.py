f094dffc599df0f2a305edf56bbaf82fc8570278
Stakkeland <sawyerstakkeland@gmail.com>
2024-05-17 18:55:10 -0700

    Make UI look better and added functionality for advanced view capabilities

--- database.py
+++ database.py
@@ -24,6 +24,7 @@ def clear_entries():
     year_entry.delete(0, tk.END)
     rating_entry.delete(0, tk.END)
     deleted_movie_entry.delete(0, tk.END)
+    db_search_entry.delete(0,tk.END)
 
 def close_program():
     '''When End button is pressed program closes'''
@@ -31,8 +32,14 @@ def close_program():
 
 def view_db():
     '''When view button is pressed, the console displays the db info in a neat format'''
-    for row in cur.execute("SELECT * FROM movie"):
-        print(f"{row[0]} | {row[1]} | {row[2]}")
+    rating = db_search_entry.get()
+
+    if rating == "":
+        for row in cur.execute("SELECT * FROM movie"):
+            print(f"{row[0]} | {row[1]} | {row[2]}")
+    else:
+        for row in cur.execute("SELECT * FROM movie WHERE score > (?)", (rating,)):
+            print(f"{row[0]} | {row[1]} | {row[2]}")
 
 def delete_column(event=None):
     '''When the user enters the name of a title and submits, the row is deleted from the db.'''
@@ -44,34 +51,38 @@ def delete_column(event=None):
 root = tk.Tk()
 root.title("Movie Input Form")
 
-tk.Label(root, text="Movie Name:").grid(row=0, column=0)
-tk.Label(root, text="Movie Year:").grid(row=1, column=0)
-tk.Label(root, text="Rating (1-10):").grid(row=2, column=0)
+tk.Label(root, text="Movie Name:").grid(row=1, column=0)
+tk.Label(root, text="Movie Year:").grid(row=2, column=0)
+tk.Label(root, text="Rating (1-10):").grid(row=3, column=0)
+tk.Label(root, text="Empty = all | score = all > score: ").grid(row=1, column=2)
+tk.Label(root, text="Movie title to remove from database: ").grid(row=3, column=2)
 
 name_entry = tk.Entry(root)
 year_entry = tk.Entry(root)
 rating_entry = tk.Entry(root)
 deleted_movie_entry = tk.Entry(root)
+db_search_entry = tk.Entry(root)
 
-name_entry.grid(row=0, column=1)
-year_entry.grid(row=1, column=1)
-rating_entry.grid(row=2, column=1)
-deleted_movie_entry.grid(row=5, column=2)
+name_entry.grid(row=1, column=1)
+year_entry.grid(row=2, column=1)
+rating_entry.grid(row=3, column=1)
+deleted_movie_entry.grid(row=3, column=3)
+db_search_entry.grid(row=1, column=3)
 
 submit_button = tk.Button(root, text="Submit", command=submit_action)
-submit_button.grid(row=3, column=1)
+submit_button.grid(row=4, column=1)
 
 clear_button = tk.Button(root, text="Clear", command=clear_entries)
-clear_button.grid(row=3, column=0)
+clear_button.grid(row=4, column=0)
 
 exit_button = tk.Button(root, text="Exit", command=close_program, bg='red')
-exit_button.grid(row=4, column=1)
+exit_button.grid(row=7, column=2)
 
 view_button = tk.Button(root, text="View db", command=view_db)
-view_button.grid(row=3, column=2)
+view_button.grid(row=2, column=3)
 
 delete_button = tk.Button(root, text="Delete column", command=delete_column)
-delete_button.grid(row=4, column=2)
+delete_button.grid(row=4, column=3)
 
 
 root.bind('<Return>', submit_action)
